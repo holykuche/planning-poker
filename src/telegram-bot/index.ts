24fec585e659c39431d0e37555de0b45c0ac45f9
Nikita Temnikov <nt.kooch@gmail.com>
2023-01-19 09:17:06 +0300

    Moved content from telegram-bot/bot.ts to telegram-bot/index.ts

--- src/telegram-bot/index.ts
+++ src/telegram-bot/index.ts
@@ -1,6 +1,13 @@
 import "reflect-metadata";
+import { Observable } from "rxjs";
+import TelegramBot, { Message } from "node-telegram-bot-api";
+
 import subscriptionConstructors from "./subscriptions";
-import bot, { messages$ } from "./bot";
+
+const bot = new TelegramBot(TELEGRAM_BOT_API_TOKEN, { polling: true });
+const messages$ = new Observable<Message>(subscriber => {
+    bot.on("message", msg => subscriber.next(msg));
+});
 
 Object.values(subscriptionConstructors)
     .map(SubscriptionConstructor => new SubscriptionConstructor(messages$, bot))
