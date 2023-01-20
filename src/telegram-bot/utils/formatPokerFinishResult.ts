ff4aa8e73a305aae614e42cc315fadd5ebcd93cf
Nikita Temnikov <nt.kooch@gmail.com>
2023-01-20 08:43:41 +0300

    Fixed poker finish result formatting

--- src/telegram-bot/utils/formatPokerFinishResult.ts
+++ src/telegram-bot/utils/formatPokerFinishResult.ts
@@ -3,7 +3,7 @@ import formatPokerResult from "./formatPokerResult";
 
 export default function(items: PokerResultItemDto[], telegramUserId: number): string {
     if (!items.length) {
-        return "Unanimously!";
+        return "Unanimously\\!";
     }
 
     return "Explain your choice:\n"
