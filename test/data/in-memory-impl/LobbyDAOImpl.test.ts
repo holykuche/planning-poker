479ffd837e7a99e68c01d36d2049e2cc2d3a76e8
Nikita Temnikov <nt.kooch@gmail.com>
2023-01-29 20:34:59 +0300

    Improved LobbyDAOImpl's getById and getByName tests

--- test/data/in-memory-impl/LobbyDAOImpl.test.ts
+++ test/data/in-memory-impl/LobbyDAOImpl.test.ts
@@ -1,7 +1,5 @@
 import "reflect-metadata";
-
-import LobbyState from "data/enum/LobbyState";
-
+import { LobbyState } from "data/enum";
 import LobbyDAOImpl from "data/in-memory-impl/LobbyDAOImpl";
 
 describe("data/in-memory-impl/LobbyDAOImpl", () => {
@@ -20,8 +18,7 @@ describe("data/in-memory-impl/LobbyDAOImpl", () => {
     });
 
     it("save should assign ID to lobby without ID", () => {
-        const lobby = { name: "dummy name", state: LobbyState.Waiting, currentTheme: "dummy theme" };
-        const storedLobby = lobbyDAO.save(lobby);
+        const storedLobby = lobbyDAO.save({ name: "dummy name", state: LobbyState.Waiting, currentTheme: "dummy theme" });
 
         expect(storedLobby.id).toBeDefined();
     });
@@ -36,10 +33,18 @@ describe("data/in-memory-impl/LobbyDAOImpl", () => {
     });
 
     it("getById should return stored lobby", () => {
-        const storedLobby = lobbyDAO.save({ name: "dummy name", state: LobbyState.Waiting });
-        const receivedLobby = lobbyDAO.getById(storedLobby.id);
+        const lobbies = [
+            { name: "dummy name 1", state: LobbyState.Waiting, currentTheme: "dummy theme 1" },
+            { name: "dummy name 2", state: LobbyState.Waiting, currentTheme: "dummy theme 2" },
+            { name: "dummy name 3", state: LobbyState.Waiting, currentTheme: "dummy theme 3" },
+            { name: "dummy name 4", state: LobbyState.Waiting, currentTheme: "dummy theme 4" },
+            { name: "dummy name 5", state: LobbyState.Waiting, currentTheme: "dummy theme 5" },
+        ];
 
-        expect(receivedLobby).toEqual(storedLobby);
+        lobbies
+            .map(lobby => lobbyDAO.save(lobby))
+            .map(storedLobby => [ storedLobby, lobbyDAO.getById(storedLobby.id) ])
+            .forEach(([ storedLobby, receivedLobby ]) => expect(receivedLobby).toEqual(storedLobby));
     });
 
     it("getById should return a new object", () => {
@@ -56,10 +61,18 @@ describe("data/in-memory-impl/LobbyDAOImpl", () => {
     });
 
     it("getByName should return stored lobby", () => {
-        const storedLobby = lobbyDAO.save({ name: "dummy name", state: LobbyState.Waiting });
-        const receivedLobby = lobbyDAO.getByName(storedLobby.name);
+        const lobbies = [
+            { name: "dummy name 1", state: LobbyState.Waiting, currentTheme: "dummy theme 1" },
+            { name: "dummy name 2", state: LobbyState.Waiting, currentTheme: "dummy theme 2" },
+            { name: "dummy name 3", state: LobbyState.Waiting, currentTheme: "dummy theme 3" },
+            { name: "dummy name 4", state: LobbyState.Waiting, currentTheme: "dummy theme 4" },
+            { name: "dummy name 5", state: LobbyState.Waiting, currentTheme: "dummy theme 5" },
+        ];
 
-        expect(receivedLobby).toEqual(storedLobby);
+        lobbies
+            .map(lobby => lobbyDAO.save(lobby))
+            .map(storedLobby => [ storedLobby, lobbyDAO.getByName(storedLobby.name) ])
+            .forEach(([ storedLobby, receivedLobby ]) => expect(receivedLobby).toEqual(storedLobby));
     });
 
     it("getByName shouldn't return not stored lobby", () => {
