4a5407885ed47bfe85c24dee90c823398b1a7eea
Nikita Temnikov <nt.kooch@gmail.com>
2023-05-09 15:57:08 +0300

    poker: Prettied ExistingRecordsError message

--- poker/src/migration/error/UnsupportedMigrationOperation.ts
+++ poker/src/migration/error/UnsupportedMigrationOperation.ts
@@ -4,6 +4,7 @@ export default class UnsupportedMigrationOperation extends Error {
 
     constructor(operation: MigrationOperation) {
         super(`Migration operation ${ operation } isn't supported`);
+        Object.setPrototypeOf(this, UnsupportedMigrationOperation.prototype);
     }
 
 }
