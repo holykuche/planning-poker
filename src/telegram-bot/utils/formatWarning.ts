af6df8212222c0e8185c5c7777eba93cff29e6c3
Nikita Temnikov <nt.kooch@gmail.com>
2024-01-27 20:15:06 +0600

    Fixed transpilation

--- src/telegram-bot/utils/formatWarning.ts
+++ src/telegram-bot/utils/formatWarning.ts
@@ -6,7 +6,10 @@ const NAMES_REGEXP = /(".+?")|('.+?')/g;
 
 export default function (text: string): string {
     const escapedText = escape(text);
-    const formattedText = (escapedText.match(NAMES_REGEXP) || [])
+
+    const matchArray: string[] = escapedText.match(NAMES_REGEXP) || [];
+
+    const formattedText = matchArray
         .reduce((res, name) => res.replace(name, italic(name)), escapedText);
     return `${ Emoji.Warning } ${ formattedText }`;
 }
