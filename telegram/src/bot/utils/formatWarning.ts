ee93234d0f20d6939d62d730ccd2786166cfbdbd
Nikita Temnikov <nikita@investbanq.com>
2024-10-19 22:08:57 +0500

    telegram: Fixed NPE

--- telegram/src/bot/utils/formatWarning.ts
+++ telegram/src/bot/utils/formatWarning.ts
@@ -7,7 +7,7 @@ const NAMES_REGEXP = /(".+?")|('.+?')/g;
 
 export default function (text: string): string {
   const escapedText = escape(text);
-  const formattedText = (escapedText.match(NAMES_REGEXP).slice() || []).reduce(
+  const formattedText = (escapedText.match(NAMES_REGEXP)?.slice() || []).reduce(
     (res, name) => res.replace(name, italic(name)),
     escapedText
   );
