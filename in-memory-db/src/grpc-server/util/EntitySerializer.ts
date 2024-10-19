d648a4d072e6511787123ee36187b7a5b3ecf6c9
Nikita Temnikov <nikita@investbanq.com>
2024-10-19 21:31:36 +0500

    in-memory-db: Improved EntitySerializer

--- in-memory-db/src/grpc-server/util/EntitySerializer.ts
+++ in-memory-db/src/grpc-server/util/EntitySerializer.ts
@@ -15,47 +15,54 @@ const isProtobufBoolean = (value: any): value is Protobuf.BoolValue => {
   return typeof value.bool_value === 'boolean';
 };
 
-export default {
-  serialize: <T extends object>(entity?: T): Protobuf.Entity<T> => {
+export default class EntitySerializer {
+  static serialize<T extends object>(entity?: T): Protobuf.Entity<T> {
     if (!entity) {
       return {} as Protobuf.Entity<T>;
     }
 
-    return Object.entries(entity).reduce(
-      (pe, [key, value]) => ({
-        ...pe,
-        [key]:
-          typeof value === 'string'
-            ? {string_value: value}
-            : typeof value === 'number'
-              ? {int_value: value}
-              : typeof value === 'boolean'
-                ? {bool_value: value}
-                : null,
-      }),
-      {} as Protobuf.Entity<T>
-    );
-  },
+    return Object.entries(entity).reduce((pe, [key, value]) => {
+      pe[key] = EntitySerializer.serializeValue(value as T[keyof T]);
+      return pe;
+    }, {} as Protobuf.Entity<T>);
+  }
 
-  deserialize: <T extends object>(protobufEntity?: Protobuf.Entity<T>): T => {
+  static serializeValue<T extends object, K extends keyof T>(
+    value: T[K]
+  ): Protobuf.Value<T, K> {
+    return (typeof value === 'string'
+      ? {string_value: value}
+      : typeof value === 'number'
+        ? {int_value: value}
+        : typeof value === 'boolean'
+          ? {bool_value: value}
+          : null) as unknown as Protobuf.Value<T, K>;
+  }
+
+  static deserialize<T extends object>(protobufEntity?: Protobuf.Entity<T>): T {
     if (!protobufEntity) {
       return null;
     }
 
     return Object.entries<Protobuf.Entity<T>[keyof Protobuf.Entity<T>]>(
       protobufEntity
-    ).reduce(
-      (e, [key, value]) => ({
-        ...e,
-        [key]: isProtobufString(value)
-          ? value.string_value
-          : isProtobufNumber(value)
-            ? value.int_value
-            : isProtobufBoolean(value)
-              ? value.bool_value
-              : null,
-      }),
-      {} as T
-    );
-  },
-};
+    ).reduce((e, [key, value]) => {
+      e[key] = EntitySerializer.deserializeValue<T, keyof T>(value);
+      return e;
+    }, {} as T);
+  }
+
+  static deserializeValue<T extends object, K extends keyof T>(
+    protobufValue: Protobuf.Value<T, K>
+  ): T[K] {
+    return (
+      isProtobufString(protobufValue)
+        ? protobufValue.string_value
+        : isProtobufNumber(protobufValue)
+          ? protobufValue.int_value
+          : isProtobufBoolean(protobufValue)
+            ? protobufValue.bool_value
+            : null
+    ) as T[K];
+  }
+}
